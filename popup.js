9b20f3c465d2f2be975eb71a93a41f4974c933b7
alexhristov23 <103336233+alexhristov23@users.noreply.github.com>
2023-08-15 12:06:35 +0100

    Delete popup.js

--- popup.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var openChatbotButton = document.getElementById('open-chatbot');
-    openChatbotButton.addEventListener('click', function() {
-      chrome.tabs.create({ url: 'http://127.0.0.1:5000/' });
-    });
-  });
-  
